6b7e09eee7184265e47726a44963e41fa35bb9ef
Hrushikesh Patil <60781042+hrushispatil19@users.noreply.github.com>
2022-09-12 21:26:11 +0530

    Update ConstructorDIApp.java

--- SpringDI/src/main/java/com/di/constructorDI/ConstructorDIApp.java
+++ SpringDI/src/main/java/com/di/constructorDI/ConstructorDIApp.java
@@ -3,19 +3,14 @@ package com.di.constructorDI;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
 public class ConstructorDIApp 
 {
     public static void main( String[] args )
     {
-        @SuppressWarnings("resource")
-		ApplicationContext context = new ClassPathXmlApplicationContext("di_config.xml");
-		/*
-		 * Employee e =new Employee(); e.setId(101); e.getEmpDetails();
-		 */
+        ApplicationContext context = new ClassPathXmlApplicationContext("constructor_config.xml");
+	/* Without Spring :
+	 * Employee e =new Employee(); e.setId(101); e.getEmpDetails();
+	 */
         Employee employee = context.getBean("emp",Employee.class);
         employee.getEmpDetails();
     }
