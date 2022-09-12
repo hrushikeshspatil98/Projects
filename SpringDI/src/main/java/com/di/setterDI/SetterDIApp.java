95f5a0d17de83db459d46ff6fc423b088a4a8e41
Hrushikesh Patil <60781042+hrushispatil19@users.noreply.github.com>
2022-09-12 21:27:24 +0530

    Update SetterDIApp.java

--- SpringDI/src/main/java/com/di/setterDI/SetterDIApp.java
+++ SpringDI/src/main/java/com/di/setterDI/SetterDIApp.java
@@ -7,11 +7,10 @@ public class SetterDIApp
 {
     public static void main( String[] args )
     {
-        @SuppressWarnings("resource")
-		ApplicationContext context = new ClassPathXmlApplicationContext("di_config.xml");
-		/*
-		 * Employee e =new Employee(); e.setId(101); e.getEmpDetails();
-		 */
+        ApplicationContext context = new ClassPathXmlApplicationContext("setter_config.xml");
+	/*  Without Spring :
+	 * Employee e =new Employee(); e.setId(101); e.getEmpDetails();
+	 */
         Employee employee = context.getBean("emp",Employee.class);
         employee.getEmpDetails();
     }
