031482e05401e9d3d1b69a7aa378b85880828c07
Hrushikesh Patil <60781042+hrushispatil19@users.noreply.github.com>
2022-09-12 21:19:46 +0530

    Create Address.java

---
+++ SpringDI/src/main/java/com/di/setterDI/Address.java
@@ -0,0 +1,33 @@
+package com.di.setterDI;
+
+public class Address {
+	
+	private String city;
+	private String state;
+	private String zip;
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	
+	@Override
+	public String toString() {
+		return "Address [city=" + city + ", state=" + state + ", zip=" + zip + "]";
+	}
+	
+}
